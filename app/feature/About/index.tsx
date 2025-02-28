e395f4c0d968e4fa642bb82939744093d9d3fd67
LAPTOP-TPVRTPQA\HP <khanakorn01@gmail.com>
2025-02-28 15:32:09 +0700

    .

--- app/feature/About/index.tsx
+++ app/feature/About/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
+import Head from "next/head";
 
 const IndexAbout = () => {
-  return <BackgroundBeamsDemo />;
+  return (
+    <>
+      <Head>
+        <title>About</title>
+        <meta property="og:title" content="About" key="title" />
+      </Head>
+      <BackgroundBeamsDemo />
+    </>
+  );
 };
 
 export default IndexAbout;
