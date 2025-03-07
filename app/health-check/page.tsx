7548342272398e7ea4c77fa35e662fe122b16eb3
LAPTOP-TPVRTPQA\HP <khanakorn01@gmail.com>
2025-03-07 16:11:56 +0700

    first ci/cd

---
+++ app/health-check/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>health-check</div>;
+};
+
+export default Page;
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
-
+import dotenv from "dotenv";
+import path from "path";
+//
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, "./env/.env.local") });
+//
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   images: {
     domains: ["assets.aceternity.com", "images.unsplash.com"],
+  },
+  env: {
+    NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+    NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+    NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
   },
   eslint: {
     ignoreDuringBuilds: true, // ✅ ข้าม ESLint เมื่อ build
